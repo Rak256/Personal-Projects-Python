03d8d1697186ef4ab9b1aeceb5d64adae4042ef6
Rakshit <102043646+Rak256@users.noreply.github.com>
2024-03-31 20:03:05 -0300

    Checks if a word is a pangram

---
+++ Pangram.py
@@ -0,0 +1,12 @@
+import string
+def is_pangram(str1, alphabet = string.ascii_lowercase ):
+    str1 = str1.lower()
+    for letter in alphabet:
+        if letter not in str1:
+            return False
+    
+    return True
+
+user_input = input("Enter your pangram: ")
+
+print(is_pangram(user_input))
